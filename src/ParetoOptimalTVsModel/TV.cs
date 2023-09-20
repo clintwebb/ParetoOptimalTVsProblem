40d99f16987c442cada4c50d4f6914b8b95c5a47
Brian Gorrie <brgorrie@gmail.com>
2023-09-20 14:21:25 +0800

    cleaned up the TV class

--- src/ParetoOptimalTVsModel/TV.cs
+++ src/ParetoOptimalTVsModel/TV.cs
@@ -16,18 +16,36 @@ using System.Collections.Generic;
 
 namespace ParetoOptimalTVsModel;
 
+/// <summary>
+/// Represents a Television model with various features.
+/// </summary>
+/// <remarks>
+/// Each feature is represented by an integer value. Higher values usually indicate better features.
+/// </remarks>
 public class TV
 {
-
+    /// <summary>
+    /// Gets or sets the features of the Television model.
+    /// </summary>
+    /// <value>The features as a list of integers.</value>
     public List<int> Features { get; set; }
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="TV"/> class.
+    /// </summary>
+    /// <param name="features">The features to initialize the TV with.</param>
+    /// <remarks>
+    /// If the provided feature list is null, an empty list will be used.
+    /// </remarks>
     public TV(List<int> features)
     {
         Features = features ?? new List<int>();
     }
 
-    // Optionally, you can add additional properties, methods or helper functions
-    // e.g., ToString override for easier debugging or display.
+    /// <summary>
+    /// Returns a string representation of the Television model's features.
+    /// </summary>
+    /// <returns>A comma-separated string of the TV's features.</returns>
     public override string ToString()
     {
         return string.Join(", ", Features);
