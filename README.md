c8a66aa5f861f6da258a289979ae7ff21ab839dd
Brian Gorrie <brgorrie@gmail.com>
2023-09-20 14:39:34 +0800

    final commit before publishing.

--- README.md
+++ README.md
@@ -12,3 +12,50 @@ dominated by any TV.
 2. Write a function to check whether one TV dominates another. 
 3. Write a function to find the pareto-optimal subset of TVs from a provided list. 
 Bonus: What if comparing TV scores was computationally expensive?
+
+
+To run this solution check it out locally then do the following 
+
+```
+dotnet restore
+dotnet build
+dotnet test 
+```
+Then to run it you can exexcute the program directly
+
+```
+C:\git\ParetoOptimalTVsProblem>.\src\ParetoOptimalTVsProblem\bin\Debug\net7.0\ParetoOptimalTVsProblem.exe
+Original list of TVs:
+1, 2, 7
+0, 3, 8
+6, 6, 2
+7, 4, 8
+0, 3, 9
+2, 5, 8
+5, 4, 3
+4, 2, 9
+7, 10, 4
+7, 3, 0
+Pareto-optimal TVs:
+7, 4, 8
+0, 3, 9
+2, 5, 8
+4, 2, 9
+7, 10, 4
+```
+
+or you can pass in the number of TVs (-t) or the number of features (-f) or both.
+
+```
+C:\git\ParetoOptimalTVsProblem>.\src\ParetoOptimalTVsProblem\bin\Debug\net7.0\ParetoOptimalTVsProblem.exe -t 5 -f 2
+Original list of TVs:
+0, 1
+3, 1
+3, 6
+2, 9
+0, 4
+Pareto-optimal TVs:
+3, 6
+2, 9
+```
+
--- src/ParetoOptimalTVsProblem/Program.cs
+++ src/ParetoOptimalTVsProblem/Program.cs
@@ -22,9 +22,38 @@ namespace ParetoOptimalTVsProblem;
 
 public class Program
 {
+
+    /// <summary>
+    /// The entry point for the application.
+    /// </summary>
+    /// <param name="args">Command-line arguments.</param>
     static void Main(string[] args)
     {
+        // Initialize default values for the number of TVs and features
+        var numberOfTVs = 10;
+        var numberOfFeatures = 3;
 
+        // Parse command-line arguments to override default values
+        // -t for setting the number of TVs
+        // -f for setting the number of features
+        for (var i = 0; i < args.Length; i++)
+        {
+            switch (args[i])
+            {
+                case "-t" when i < args.Length - 1:
+                    // Try to parse the next argument as the number of TVs
+                    int.TryParse(args[i + 1], out numberOfTVs);
+                    i++;  // Skip the next argument since we've processed it
+                    break;
+                case "-f" when i < args.Length - 1:
+                    // Try to parse the next argument as the number of features
+                    int.TryParse(args[i + 1], out numberOfFeatures);
+                    i++;  // Skip the next argument since we've processed it
+                    break;
+            }
+        }
+
+        // Initialize dependency injection container using Autofac
         var builder = new ContainerBuilder();
         builder.RegisterType<DominanceChecker>().AsSelf();
         builder.RegisterType<TVGenerator>().AsSelf();
@@ -32,27 +61,30 @@ public class Program
             .WithParameter(new TypedParameter(typeof(DominanceChecker), new DominanceChecker()));
         var container = builder.Build();
 
-        using (var scope = container.BeginLifetimeScope())
+        // Begin dependency injection scope
+        using var scope = container.BeginLifetimeScope();
+
+        // Resolve dependencies
+        var dominanceChecker = scope.Resolve<DominanceChecker>();
+        var tvGenerator = scope.Resolve<TVGenerator>();
+        var paretoFinder = scope.Resolve<ParetoFinder>();
+
+        // Generate a list of random TVs based on the provided (or default) parameters
+        var tvs = tvGenerator.GenerateRandomTVs(numberOfTVs, numberOfFeatures);
+
+        // Display the original list of TVs
+        Console.WriteLine("Original list of TVs:");
+        foreach (var tv in tvs)
         {
-            var dominanceChecker = scope.Resolve<DominanceChecker>();
-            var tvGenerator = scope.Resolve<TVGenerator>();
-            var paretoFinder = scope.Resolve<ParetoFinder>();
+            Console.WriteLine(string.Join(", ", tv.Features));
+        }
 
-            var tvs = tvGenerator.GenerateRandomTVs(10, 3);
-
-            Console.WriteLine("Original list of TVs:");
-            foreach (var tv in tvs)
-            {
-                Console.WriteLine(string.Join(", ", tv.Features));
-            }
-
-            var paretoOptimalTVs = paretoFinder.FindParetoOptimal(tvs);
-
-            Console.WriteLine("Pareto-optimal TVs:");
-            foreach (var tv in paretoOptimalTVs)
-            {
-                Console.WriteLine(string.Join(", ", tv.Features));
-            }
+        // Find and display the Pareto-optimal TVs from the original list
+        var paretoOptimalTVs = paretoFinder.FindParetoOptimal(tvs);
+        Console.WriteLine("Pareto-optimal TVs:");
+        foreach (var tv in paretoOptimalTVs)
+        {
+            Console.WriteLine(string.Join(", ", tv.Features));
         }
 
     }
